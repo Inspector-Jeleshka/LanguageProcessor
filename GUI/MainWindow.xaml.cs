edfc582e8f918e400d7e231c953e007f33c77bb2
Jeleshka <haidendark1236@gmail.com>
2026-02-13 11:30:48 +0700

    Применил file scoped namespace

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -9,17 +9,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GUI
-{
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+namespace GUI;
 
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
 	}
+
 }
