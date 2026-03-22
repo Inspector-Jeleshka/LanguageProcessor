75e580629df972638fcdec7a3ab0a09878ef4990
Jeleshka <haidendark1236@gmail.com>
2026-03-22 23:48:52 +0700

    Добавил сущность для вывода данных о лексеме

---
+++ GUI/LexemeInfo.cs
@@ -0,0 +1,33 @@
+using LexicalAnalyzer;
+using LexicalAnalyzer.Tokens;
+using System.ComponentModel;
+
+namespace GUI;
+
+public class LexemeInfo
+{
+	public int Code { get; }
+	public string Type { get; }
+	public string Value { get; }
+	public string Location => $"Линия {Line}, {Columns.Start}-{Columns.End}";
+	public int Line { get; }
+	public (int Start, int End) Columns { get; }
+
+	public LexemeInfo(IToken token)
+	{
+		Code = token.Code;
+		Type = token.Name;
+		Value = token.ToString();
+		Line = token.Line;
+		Columns = token.Columns;
+	}
+	public LexemeInfo(ScannerException exception)
+	{
+		Code = -1;
+		Type = "Ошибка";
+		Value = exception.Message;
+		Line = exception.Line;
+		Columns = exception.Columns;
+	}
+
+}
