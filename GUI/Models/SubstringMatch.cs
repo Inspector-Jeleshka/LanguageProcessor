b33082cefc2df74fb8a50047ad48273c48ce64d9
Jeleshka <haidendark1236@gmail.com>
2026-04-18 00:09:09 +0700

    refactor: Расширил интерфейс SubstringMatch

--- GUI/Models/SubstringMatch.cs
+++ GUI/Models/SubstringMatch.cs
@@ -2,19 +2,17 @@ using System.Text.RegularExpressions;
 
 namespace GUI.Models;
 
-public class SubstringMatch
+public record SubstringMatch(string Value, int Index, int Length, int Line, (int Start, int End) Columns)
 {
-	private readonly Match _match;
-
-	public string Value => _match.Value;
-	public int Index => _match.Index;
-	public int Length => _match.Length;
+	public string Value { get; init; } = Value;
+	public int Index { get; init; } = Index;
+	public int Length { get; init; } = Length;
 	public string Location => $"Линия {Line}, {Columns.Start}-{Columns.End}";
-	public int Line { get; }
-	public (int Start, int End) Columns { get; }
+	public int Line { get; init; } = Line;
+	public (int Start, int End) Columns { get; init; } = Columns;
 
 	public SubstringMatch(Match match, int line, (int Start, int End) columns)
-		=> (_match, Line, Columns) = (match, line, columns);
+		: this(match.Value, match.Index, match.Length, line, columns) { }
 
 	public override string ToString() => Value;
 }
