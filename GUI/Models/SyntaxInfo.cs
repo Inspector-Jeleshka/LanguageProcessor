115277b40f456b6f7696d043139966e78d642d38
Jeleshka <haidendark1236@gmail.com>
2026-04-20 14:33:05 +0700

    feat: Добавил модель результата парсера

---
+++ GUI/Models/SyntaxInfo.cs
@@ -0,0 +1,6 @@
+﻿namespace GUI.Models;
+
+public record SyntaxInfo(string Value, int Line, (int Start, int End) Columns, string Description)
+{
+	public string Location => $"Линия {Line}, {Columns.Start}-{Columns.End}";
+}
