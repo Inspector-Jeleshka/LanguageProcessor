162ac3020e657afbfb6851beb01d29dd595e737b
Jeleshka <haidendark1236@gmail.com>
2026-04-23 00:35:55 +0700

    wip: Определил состояния парсера, парсер

--- SyntaxAnalyzer/Parser.cs
+++ SyntaxAnalyzer/Parser.cs
@@ -3,41 +3,50 @@ using LexicalAnalyzer.Tokens;
 
 namespace SyntaxAnalyzer;
 
-//public enum ParserState
-//{
-//	Start,
-//	Id,
-//	IdEnd,
-//	Type,
-//	Assignment,
-//	Float,
-//	Int,
-//	Fraction,
-//	End,
-//}
+public interface IParserState
+{
+	IParserState Match(IToken token);
+}
 
-public record ParserState;
+public interface IParserState<TParserState> : IParserState
+	where TParserState : IParserState
+{
+	new TParserState Match(IToken token);
+	IParserState IParserState.Match(IToken token) => Match(token);
+}
 
-public record ConstState : ParserState;
-public record SpaceState : ParserState;
-public record IdentifierState : ParserState;
-public record ColonState : ParserState;
-public record F32State : ParserState;
-public record AssignmentState : ParserState;
-public record NumberState : ParserState;
-public record SemicolonState : ParserState;
+public record ConstState : IParserState<SpaceState>
+{
+	public SpaceState Match(IToken token) => token switch
+	{
+		Space space => new SpaceState(),
+		_ => throw new NotImplementedException(),
+	};
+}
+public record SpaceState : IParserState<IdentifierState>;
+public record IdentifierState : IParserState<ColonState>;
+public record ColonState : IParserState<F32State>;
+public record F32State : IParserState<AssignmentState>;
+public record AssignmentState : IParserState<NumberState>;
+public record NumberState : IParserState<SemicolonState>;
+public record SemicolonState : IParserState<ConstState>;
+
+public record ParseError(string Value, int Line, (int Start, int End) Columns, string Description);
 
 public class Parser
 {
-	private ParserState _state;
+	private IParserState _state;
 
 	public int Line { get; private set; }
 	public int Column { get; private set; }
 
-	public bool TryParse(IEnumerable<IToken> tokens)
+	public bool TryParse(IEnumerable<IToken> tokens, out List<ParseError> errors)
 	{
 		Line = 1;
 		Column = 1;
+
+		IParserState state = new ConstState();
+		state.Match()
 
 		throw new NotImplementedException();
 	}
